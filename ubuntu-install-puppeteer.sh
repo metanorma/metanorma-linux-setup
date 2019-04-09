d2f9d7092518a7332e2681bbda5b37092fb396ad
Alexande B <abobrikovich@gmail.com>
2019-04-09 17:50:19 +0300

    Don't install npm if exists

--- ubuntu-install-puppeteer.sh
+++ ubuntu-install-puppeteer.sh
@@ -7,7 +7,8 @@ fi
 
 # Install puppeteer
 echo '[node: puppeteer] Installing prerequisites...'
-apt-get install -y npm gconf-service gconf-service-backend gconf2-common libdbus-glib-1-2
+command -v npm >/dev/null 2>&1 || { apt-get install -y npm; }
+apt-get install -y gconf-service gconf-service-backend gconf2-common libdbus-glib-1-2
 apt-get install -y libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libgconf2-4 libasound2 libatk1.0-0 libgtk-3-0
 
 echo '[node: puppeteer] Installing puppeteer...'
