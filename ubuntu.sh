b0df8c1763a837b241d958e7273ffd399adcb9d7
Alexande B <abobrikovich@gmail.com>
2019-04-10 15:45:24 +0300

    Add nvm node alias

--- ubuntu.sh
+++ ubuntu.sh
@@ -30,6 +30,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
+nvm alias mn-node node
 
 # Install puppeteer
 curl -L "https://raw.githubusercontent.com/metanorma/metanorma-linux-setup/master/ubuntu-install-puppeteer.sh" | bash
