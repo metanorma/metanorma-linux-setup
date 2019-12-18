6f3d094ed5e1cd234d1f57b07fd1b005dee029a2
Ronald Tse <ronald.tse@ribose.com>
2019-12-18 12:27:38 +0800

    Update RUBY_VER to 2.6.6 and remove nokogiri version restriction

--- install-gems.sh
+++ install-gems.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
-RUBY_VER="${RUBY_VER:-2.5.1}"
+RUBY_VER="${RUBY_VER:-2.6.5}"
 
 if [ -f "/etc/profile.d/rvm.sh" ]; then
   echo "[rvm] Already installed."
 else
   echo "[rvm] Installing RVM..."
   curl -sSL https://get.rvm.io | bash
+  usermod -a -G rvm `whoami`
 fi
 
 # This is if RVM got installed somewhere else
@@ -19,6 +20,6 @@ rvm use ${RUBY_VER} --install --binary --fuzzy
 # Ruby sassc gem depends on rake gem
 gem install rake
 gem install bundler
-gem install nokogiri -v '1.8.4'
+gem install nokogiri
 gem install metanorma
 gem install metanorma-cli
