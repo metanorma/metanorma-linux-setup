c4dc162da713d8d088c105e47ee0440e8320a3f2
Ronald Tse <ronald.tse@ribose.com>
2019-12-18 12:28:12 +0800

    Ubuntu install libX11-6 to prevent https://stackoverflow.com/questions/56478579/error-when-running-headless-chrome-in-jenkins-docker-using-puppeteer

--- ubuntu-install-puppeteer.sh
+++ ubuntu-install-puppeteer.sh
@@ -9,7 +9,7 @@ fi
 echo '[node: puppeteer] Installing prerequisites...'
 command -v npm >/dev/null 2>&1 || { apt-get install -y npm; }
 apt-get install -y gconf-service gconf-service-backend gconf2-common libdbus-glib-1-2
-apt-get install -y libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libgconf2-4 libasound2 libatk1.0-0 libgtk-3-0
+apt-get install -y libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libgconf2-4 libasound2 libatk1.0-0 libgtk-3-0 libx11-6
 
 echo '[node: puppeteer] Installing puppeteer...'
 npm install -g --save --save-exact puppeteer --unsafe-perm=true
