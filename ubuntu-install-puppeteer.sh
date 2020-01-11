2a91e267e7a296a1fc692268ec10c4b196e79367
Alexande B <abobrikovich@gmail.com>
2020-01-11 12:14:58 +0300

    Update puppeteer dependency list

--- ubuntu-install-puppeteer.sh
+++ ubuntu-install-puppeteer.sh
@@ -8,8 +8,9 @@ fi
 # Install puppeteer
 echo '[node: puppeteer] Installing prerequisites...'
 command -v npm >/dev/null 2>&1 || { apt-get install -y npm; }
-apt-get install -y gconf-service gconf-service-backend gconf2-common libdbus-glib-1-2
-apt-get install -y libpangocairo-1.0-0 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libnss3 libcups2 libxss1 libxrandr2 libgconf2-4 libasound2 libatk1.0-0 libgtk-3-0 libx11-6
+
+# https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#chrome-headless-doesnt-launch-on-unix
+apt-get install -y gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 
 echo '[node: puppeteer] Installing puppeteer...'
 npm install -g --save --save-exact puppeteer --unsafe-perm=true
